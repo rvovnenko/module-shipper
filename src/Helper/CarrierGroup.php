03e66c78e2177d12ea6e02294b199bc87b15bd9a
Roman Vovnenko <roman.vovnenko@acidgreen.com.au>
2019-06-28 00:54:09 +1000

    debug 1

--- src/Helper/CarrierGroup.php
+++ src/Helper/CarrierGroup.php
@@ -138,13 +138,22 @@ class CarrierGroup extends Data
      */
     public function saveCarrierGroupInformation($shippingAddress, $shippingMethod, array $additionalDetail = [])
     {
+	
+        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/rrr.log');
+		$logger = new \Zend\Log\Logger();
+		$logger->addWriter($writer);
+		$logger->info(__METHOD__);
+
         //admin and front end orders use method
         //SHQ18-1391 - need to ensure rate matches on code and price. If more than 1, then use latest
         $foundRate = false;
         $shippingPrice = $shippingAddress->getShippingAmount();
-        foreach ($shippingAddress->getShippingRatesCollection() as $rate) {
+	foreach ($shippingAddress->getShippingRatesCollection() as $rate) {
+
+	    $logger->info($rate->getCode().'-'.$shippingMethod.' | '.$rate->getPrice().'-'.$shippingPrice);
             if ($rate->getCode() == $shippingMethod && $rate->getPrice() == $shippingPrice) {
-                $foundRate = $rate;
+		    $foundRate = $rate;
+                    $logger->info('OK');
             }
         }
 
